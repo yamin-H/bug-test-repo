c4e085f79f8ead95c64d7904766b6c4e575b45c0
Yamin Hossain <168386743+yamin-H@users.noreply.github.com>
2026-06-28 11:44:39 +0600

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -5,7 +5,7 @@ def is_palindrome(s):
     return s == s[::-1]
 
 def count_vowels(s):
-    vowels = "aeiou"
+    vowels = "aeiou"  # bug: missing uppercase vowels
     count = 0
     for char in s:
         if char in vowels:
@@ -16,10 +16,10 @@ def capitalize_words(s):
     words = s.split(" ")
     result = []
     for word in words:
-        result.append(word[0].upper() + word[1:])  # bug: crashes on empty string
+        result.append(word[0].upper() + word[1:])
     return " ".join(result)
 
 def truncate(s, max_length):
     if len(s) > max_length:
-        return s[:max_length - 3] + "..."  # bug: wrong, should be max_length not max_length-3
+        return s[:max_length] + "..."  # bug: returns more than max_length chars
     return s
